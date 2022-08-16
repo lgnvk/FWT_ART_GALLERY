f6f42c72d5daeeb5eaec42fa9ff41bd44aa9bf79
Konstantin Loginov <k.s.loginoff@gmail.com>
2022-08-16 13:39:14 +0400

    Storybook and app component

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './css/styles.css';
-
-function App() {
-  return <div className="App">s</div>;
-}
-
-export default App;
---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.module.scss';
+import Card from '../Card';
+
+function App() {
+  return (
+    <div className="App">
+      s
+      <Card />
+    </div>
+  );
+}
+
+export default App;
